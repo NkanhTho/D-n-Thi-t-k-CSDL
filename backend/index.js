b2b55d2d0dc09dc122392fa46b123649657c4b63
Nguyen Khai Anh Tho <anhtho@Nguyens-MacBook-Pro.local>
2026-08-25 21:07:54 +0700

    Setup backend project structure

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Backend đang chạy!'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server chạy ở port ${PORT}`));
